257a2f3ea089ec78994977991c37b2d48728c973
luis daniel perez pinedo <luisdaperezpinedo@gmail.com>
2025-08-16 23:25:32 -0500

    feat: se agrego un boton y la funcion de aparecer y desaparecer

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+        // inyectar partes de codigo
+
+// busca al header
 fetch("heider.html")
 .then(Response => Response.text())
 .then(data => {
     document.getElementById("header-container").innerHTML = data;
 });
 
+// busca al footer
 fetch("footer.html")
 .then(Response => Response.text())
 .then(data => {
     document.getElementById("footer-container").innerHTML = data;
 });
+
+        // boton para movil
+
+const menuToggle = document.getElementById(menu-toggle);
+const navHeider = document.getElementById(nav-header);
+
+menuToggle.addEventListener("clik", () =>{
+    navHeider.classList.toggle("active");
+} )
