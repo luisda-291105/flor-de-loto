db5b96b1a2ca076d7c15a974f7c15397a89c6f7e
luis daniel perez pinedo <luisdaperezpinedo@gmail.com>
2025-08-17 10:32:16 -0500

    feat: se le dio la forma al header

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
         // inyectar partes de codigo
 
 // busca al header
-fetch("heider.html")
-.then(Response => Response.text())
+fetch("heider.html") // busca el archivo heider.html
+.then(Response => Response.text()) // lo convierte a texto (HTML)
 .then(data => {
+    // lo inserta dentro del div con id header-container
     document.getElementById("header-container").innerHTML = data;
 });
 
 // busca al footer
 fetch("footer.html")
-.then(Response => Response.text())
+.then(response => response.text())
 .then(data => {
+    
     document.getElementById("footer-container").innerHTML = data;
 });
 
         // boton para movil
 
-const menuToggle = document.getElementById(menu-toggle);
-const navHeider = document.getElementById(nav-header);
+const menuToggle = document.getElementById("menu-toggle");
+const navHeader = document.getElementById("nav-header");
 
-menuToggle.addEventListener("clik", () =>{
-    navHeider.classList.toggle("active");
+menuToggle.addEventListener("click", () =>{
+    navHeader.classList.toggle("active");
 } )
