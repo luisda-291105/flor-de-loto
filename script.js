d720649cd028dcb989603baf9f065ec012b34dab
luis daniel perez pinedo <luisdaperezpinedo@gmail.com>
2025-08-17 11:31:37 -0500

    style: se le agrego comentario

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
         // inyectar partes de codigo
 
 // busca al header
-fetch("heider.html") // busca el archivo heider.html
-.then(Response => Response.text()) // lo convierte a texto (HTML)
+fetch("heider.html")
+// lo convierte a texto (HTML) 
+.then(Response => Response.text()) 
+// lo inserta dentro del div con id header-container
 .then(data => {
-    // lo inserta dentro del div con id header-container
     document.getElementById("header-container").innerHTML = data;
+    
                 // boton para movil
-
-
 const menuToggle = document.getElementById("menu-toggle");
 const navHeader = document.getElementById("nav-header");
 
 menuToggle.addEventListener("click", () =>{
     navHeader.classList.toggle("active");
-} )
+})
 });
 
 // busca al footer
 fetch("footer.html")
+// lo convierte a texto (HTML)
 .then(response => response.text())
+// lo inserta dentro del div con id header-containe
 .then(data => {
-    
     document.getElementById("footer-container").innerHTML = data;
     
-
 });
 
 
